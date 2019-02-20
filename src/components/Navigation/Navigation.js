a3bf6bd9bc087869780270b88d339862d94bec9e
John <johnbender44@gmail.com>
2019-02-20 11:17:49 -0600

    fixing about link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => {
                 <p className="pl4">Savings Calculator by John Bender</p>
             </div>
             <div>
-                <p className="pr4"><a href="https://github.com/jbenderdev/savings-calculator/blob/master/README.md">About</a></p>
+                <p className="pr4"><a href="https://github.com/jbenderdev/savings-calculator-app">About</a></p>
             </div>
         </nav>
     )
